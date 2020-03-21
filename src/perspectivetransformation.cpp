06786148df5ea18d7ecbc53c90e5917bdc1a8516
JerryWang-xmu <1071641947@qq.com>
2020-03-21 12:13:52 +0800

    ImageOperationFunctions

---
+++ src/perspectivetransformation.cpp
@@ -0,0 +1,42 @@
+﻿#include<opencv2/opencv.hpp>
+#include<iostream>
+
+using namespace std;
+using namespace cv;
+
+#define _DST_P "e:/try/x41.png"
+class userdata {
+public:
+	vector<Point2f> points;
+};
+
+
+
+int main()
+{
+	Mat Image = imread(_DST_P);
+	imshow("Image", Image);
+	Mat result = Mat::zeros(400, 400, CV_8UC3);
+
+	vector<Point2f> obj;
+	obj.push_back(Point2f(0, 0));
+	obj.push_back(Point2f(400, 0));
+	obj.push_back(Point2f(400, 400));
+	obj.push_back(Point2f(0, 400));
+
+	Mat dst = Image.clone();
+
+	userdata data;
+	data.points.push_back(Point2f(100, 100));//此四个点应该修改为二维码最外层轮廓四个顶点，顺序为顺时针
+	data.points.push_back(Point2f(100, 800));
+	data.points.push_back(Point2f(800, 100));
+	data.points.push_back(Point2f(800, 800));
+
+
+	Mat H = findHomography(data.points, obj, RANSAC);;
+	warpPerspective(Image, result, H, result.size());
+	namedWindow("result", 0);
+	imshow("result", result);
+	imwrite("e:/result.png", result);
+	waitKey(0);
+}
